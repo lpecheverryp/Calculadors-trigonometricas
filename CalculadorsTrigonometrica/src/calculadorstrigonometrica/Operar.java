7aa61533b4929f41cb6689d15c6e891cc435e085
estudiantes <estudiantes@labing00>
2017-03-24 17:16:05 -0500

    creación interfaz gráfica

---
+++ CalculadorsTrigonometrica/src/calculadorstrigonometrica/Operar.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorstrigonometrica;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Operar {
+      double n1;
+    double n2;
+    
+    public double Sumar(){
+        
+        return(n1+n2);
+        
+    }
+    
+    public double Multiplicar(){
+        
+        return(n1*n2);
+        
+    }
+
+    public double Dividir(){
+        
+        return(n1/n2);
+        
+    }
+    
+    public double Restar(){
+        
+        return(n1-n2);
+        
+    }
+}
